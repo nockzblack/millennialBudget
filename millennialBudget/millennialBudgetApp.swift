f79b968b759b0c0c63355b2022583a9847363992
Fernando Benavides <nockzblack@gmail.com>
2022-04-20 10:35:31 -0500

    Initial Commit

---
+++ millennialBudget/millennialBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  millennialBudgetApp.swift
+//  millennialBudget
+//
+//  Created by Fernando's Mac on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct millennialBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
